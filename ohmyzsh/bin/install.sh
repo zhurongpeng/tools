c3064e4b0a2559955f49afcbe7cc4a9aced14ac6
朱荣鹏 <738592537@qq.com>
2018-07-05 23:54:13 +0800

    fxi

--- ohmyzsh/bin/install.sh
+++ ohmyzsh/bin/install.sh
@@ -6,6 +6,5 @@ sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install
 
 sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"ys\"/g' ~/.zshrc
 
-
-
+sed -i 's/plugins=(git)/plugins=(autojump composer docker gradle git git-flow git-prompt git-remote-branch git-flow-completion go golang kubectl man npm pass repo rsync sudo systemd yum)/g' ~/.zshrc
 
