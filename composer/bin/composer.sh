02e402e4604f8558ba94f82cd33d9b65e27d8e81
朱荣鹏 <738592537@qq.com>
2018-07-13 14:00:59 +0800

    fi

--- composer/bin/composer.sh
+++ composer/bin/composer.sh
@@ -2,7 +2,15 @@
 
 cd /usr/local/src/lib
 
+if [ -f "/usr/local/src/lib/composer.phar" ];then
+    rm -rf /usr/local/src/lib/composer.phar
+fi
+
 curl -sS https://getcomposer.org/installer > composer.phar
+
+if [ -f "/usr/local/bin/composer" ];then
+    rm -rf /usr/local/bin/composer
+fi
 
 ln -s /usr/local/src/lib/composer.phar /usr/local/bin/composer
 
