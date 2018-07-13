7bf14566f2670b147163df2668a3fc2b8fd83dc3
朱荣鹏 <738592537@qq.com>
2018-07-13 13:57:30 +0800

    fix

--- composer/bin/composer.sh
+++ composer/bin/composer.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-yum -y mbstring zip zlib
-
 cd /usr/local/src/lib
 
 curl -sS https://getcomposer.org/installer > composer.phar
 
-ln -s composer.phar /usr/local/bin/composer
+ln -s /usr/local/src/lib/composer.phar /usr/local/bin/composer
 
