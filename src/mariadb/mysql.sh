b1d2609aea9d36eaad086a0cbb7951f437586c9f
朱荣鹏 <738592537@qq.com>
2018-08-07 22:55:13 +0800

    fix

--- src/mariadb/mysql.sh
+++ src/mariadb/mysql.sh
@@ -25,14 +25,32 @@ if [ $count != 0 ];then
     kill $pid
 fi
 
+count=`pidof "/usr/local/mysql/bin/mysqld" | wc -l`
+
+if [ $count != 0 ];then
+    pid=`pidof "/usr/local/mysql/bin/mysqld"`
+
+    kill $pid
+fi
+
+
+# 定义安装版本号
+if [ $3 ]; then
+    version=$1
+else
+    version="10.3.8"
+fi
+
+name="mariadb"
+dirname="$name-$version"
+tarfile="$name-${version}.tar.gz"
+
 cd /usr/local
 
 if [ -d /usr/local/mysql ]
 then
     rm -rf /usr/local/mysql
 fi
-
-dir="mariadb-10.3.8"
 
 if [ ! -d "/usr/local/src/lib/" ]
 then
@@ -41,22 +59,36 @@ fi
 
 cd /usr/local/src/lib
 
-if [ ! -f /usr/local/src/lib/mariadb-10.3.8.tar.gz ]
+if [ ! -f /usr/local/src/lib/$tarfile ]
 then
-    wget https://downloads.mariadb.org/interstitial/mariadb-10.3.8/source/mariadb-10.3.8.tar.gz
+    wget https://downloads.mariadb.org/interstitial/mariadb-10.3.8/source/$tarfile
     # wget http://pkgs.fedoraproject.org/repo/pkgs/mysql/mysql-boost-5.7.16.tar.gz/f7724b816919878760b5c7c9956e6508/mysql-boost-5.7.16.tar.gz
 fi
 
-tar zxf ./mariadb-10.3.8.tar.gz
+tar zxf /usr/local/src/lib/$tarfile
 
-cd /usr/local/src/lib/$dir
+cd /usr/local/src/lib/$dirname
 
-cmake . -DCMAKE_INSTALL_PREFIX=/usr/local/mysql -DMYSQL_DATADIR=/usr/local/mysql/data -DMYSQL_UNIX_ADDR=/usr/local/mysql/tmp/mysql.sock -DMYSQL_USER=$user -DDEFAULT_CHARSET=utf8 -DDEFAULT_COLLATION=utf8_general_ci -DENABLED_LOCAL_INFILE=ON -DWITH_INNOBASE_STORAGE_ENGINE=1 -DWITH_FEDERATED_STORAGE_ENGINE=1 -DWITH_BLACKHOLE_STORAGE_ENGINE=1 -DWITHOUT_EXAMPLE_STORAGE_ENGINE=1 -DDOWNLOAD_BOOST=1
+cmake . -DCMAKE_INSTALL_PREFIX=/usr/local/mysql \
+    -DMYSQL_DATADIR=/usr/local/mysql/data \
+    -DMYSQL_UNIX_ADDR=/usr/local/mysql/tmp/mysql.sock \
+    -DMYSQL_USER=$user \
+    -DDEFAULT_CHARSET=utf8 \
+    -DDEFAULT_COLLATION=utf8_general_ci \
+    -DWITH_EXTRA_CHARSETS=all \
+    -DENABLED_LOCAL_INFILE=ON \
+    -DWITH_INNOBASE_STORAGE_ENGINE=1 \
+    -DWITH_FEDERATED_STORAGE_ENGINE=1 \
+    -DWITH_BLACKHOLE_STORAGE_ENGINE=1 \
+    -DWITHOUT_EXAMPLE_STORAGE_ENGINE=1 \
+    -DDOWNLOAD_BOOST=1
 
 make && make install
 
-cp /usr/local/mysql/support-files/mysql.server /etc/mysqld
-cp /usr/local/mysql/support-files/my-medium.cnf /etc/my.cnf
+cp /usr/local/mysql/support-files/mysql.server /etc/rc.d/init.d/mysqld
+chmod +x /etc/rc.d/init.d/mysqld
+chkconfig --add mysqld 
+cp /usr/local/mysql/support-files/my-default.cnf /etc/my.cnf
 
 # mkdir -p /data/mysql
 # /usr/local/mysql/bin/mysqld --initialize --user=mysql --basedir=/usr/local/mysql --datadir=/data/mysql
@@ -64,14 +96,6 @@ cp /usr/local/mysql/support-files/my-medium.cnf /etc/my.cnf
 /usr/local/mysql/bin/mysqld --initialize --user=$user --basedir=/usr/local/mysql
 
 echo skip-grant-tables >> /etc/my.cnf
-
-count=`pidof "/usr/local/mysql/bin/mysqld" | wc -l`
-
-if [ $count != 0 ];then
-    pid=`pidof "/usr/local/mysql/bin/mysqld"`
-
-    kill $pid
-fi
 
 /usr/local/mysql/support-files/mysql.server start
 
