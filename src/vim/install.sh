d60b1dc2b78ceacd4c9e0f9c401579aa08884ed4
朱荣鹏 <738592537@qq.com>
2018-08-07 17:36:34 +0800

    fix

--- src/vim/install.sh
+++ src/vim/install.sh
@@ -4,6 +4,11 @@ yum install vim
 
 path=$(cd `dirname $0`; pwd)
 
+if [ ! -d "/usr/local/src/lib/" ]
+then
+    mkdir /usr/local/src/lib
+fi
+
 curl -L https://raw.githubusercontent.com/kepbod/ivim/master/setup.sh > /usr/local/src/lib/setup.sh
 
 sh /usr/local/src/lib/setup.sh -i
