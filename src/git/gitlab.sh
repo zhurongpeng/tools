b6f31ff90254d16feaac45a4cf4defd1ec978b56
朱荣鹏 <738592537@qq.com>
2018-08-08 19:57:40 +0800

    fix

--- src/git/gitlab.sh
+++ src/git/gitlab.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-cd /usr/local/src
+sudo yum install -y curl policycoreutils-python openssh-server
+sudo systemctl enable sshd
+sudo systemctl start sshd
+sudo firewall-cmd --permanent --add-service=http
+sudo systemctl reload firewalld
 
-wget https://mirrors.tuna.tsinghua.edu.cn/gitlab-ce/yum/el7/gitlab-ce-8.8.5-ce.1.el7.x86_64.rpm
+sudo yum install postfix
+sudo systemctl enable postfix
+sudo systemctl start postfix
 
-rpm -ivh gitlab-ce-8.8.5-ce.1.el7.x86_64.rpm
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.rpm.sh | sudo bash
+
+sudo EXTERNAL_URL="http://git.zhurp.xyz" yum install -y gitlab-ee
+
+
+# cd /usr/local/src
+#
+# wget https://mirrors.tuna.tsinghua.edu.cn/gitlab-ce/yum/el7/gitlab-ce-8.8.5-ce.1.el7.x86_64.rpm
+#
+# rpm -ivh gitlab-ce-8.8.5-ce.1.el7.x86_64.rpm
 
 # echo external_url 'http://git.zhurp.xyz' >> /etc/gitlab/gitlab.rb
 # sed -i '42s/daemonize no/daemonize yes/' /usr/local/redis/redis.conf
