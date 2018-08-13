fa03ab6b5fa75bc8d1155bc3f755fff17990e0b2
朱荣鹏 <738592537@qq.com>
2018-08-14 03:06:27 +0800

    fix

--- src/composer/satis.sh
+++ src/composer/satis.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-if [ ! -d /data ];then
-    mkdir /data
+if [ ! -d /tool ];then
+    mkdir /tool
 fi
 
 cd /data
+
+if [ -d /tool/satis ];then
+    rm -rf /tool/satis
+fi
+
+if [ -d /tool/composer/ ];then
+    rm -rf /tool/satis
+fi
 
 composer create-project composer/satis --stability=dev --keep-vcs
 
